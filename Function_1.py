8d61046b30cfe5450b95a20f3c9f5c8a67bc4cf4
Sarthak Jain <sarthakjain7@outlook.com>
2020-09-21 16:13:08 +0530

    Completed function 1

---
+++ Function_1.py
@@ -0,0 +1,10 @@
+from win32com.client import Dispatch # used to make pronounce function using sapi voice
+
+def pronounce(str):
+    '''This function is used to make pronounce any string given to it'''
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == "__main__":
+    string=input("Enter the string you want to be pronounced\n")
+    pronounce(string)    
